1915a9a34c70edbdcbcc1d4d45a7102bfb71a7fd
jigarhira <jigarhira354@gmail.com>
2020-02-19 13:16:18 -0800

    fixed output

--- hw5/hw5_hiraj.c
+++ hw5/hw5_hiraj.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define ROWS		(int)480
 #define COLUMNS		(int)640
+#define PI 3.14159265358979323846
 
+#define sqr(x)	((x)*(x))
+
+
+void draw_line(unsigned char image[ROWS][COLUMNS], int theta, int rho);
+void hough(int x, int y, int dedx, int dedy, int array[180][400]);
 void sobel(unsigned char in_image[ROWS][COLUMNS], int out_image[ROWS][COLUMNS], int operator[3][3], int *max);
 void sgm(int ximage[ROWS][COLUMNS], int yimage[ROWS][COLUMNS], unsigned char out_image[ROWS][COLUMNS], int *max);
 void binary(unsigned char sgmimage[ROWS][COLUMNS], unsigned char out_image[ROWS][COLUMNS], int threshold);
@@ -15,10 +22,11 @@ void header( int row, int col, unsigned char head[32] );
 int main( int argc, char **argv )
 {
 
-	int				i, threshold[3], max[3];
+	int				i, j, threshold[3], max[3];
 	FILE			*fp;
 	int				ximage[ROWS][COLUMNS], yimage[ROWS][COLUMNS];
-	unsigned char	image[ROWS][COLUMNS], sgmimage[ROWS][COLUMNS], bimage[ROWS][COLUMNS], head[32];
+	int				A[180][400];	// voting poll for hough transform lines
+	unsigned char	image[ROWS][COLUMNS], sgmimage[ROWS][COLUMNS], bimage[ROWS][COLUMNS], lineimage[ROWS][COLUMNS], head[32];
 	char			filename[6][50], ifilename[50];
 
 	int sobel_x[3][3] = {
@@ -32,13 +40,10 @@ int main( int argc, char **argv )
 							{1, 2, 1}
 						};
 
-	threshold[0] = 40;
+	threshold[0] = 100;
 
 	strcpy( filename[0], "image" );
 	header ( ROWS, COLUMNS, head );
-
-
-	printf( "Maximum of Gx, Gy, SGM\n" );
 
 	clear_int( ximage );
 	clear_int( yimage );
@@ -79,6 +84,39 @@ int main( int argc, char **argv )
 	binary(sgmimage, bimage, threshold[0]);
 
 
+	/* Find the most common lines */
+	for (i = 0; i < ROWS; i++)
+	{
+		for (j = 0; j < COLUMNS; j++)
+		{
+			/* only transform edges */
+			if (sgmimage[i][j] > threshold[0])
+			{
+				/* perform hough transform and vote for parameters */
+				hough(j, i, ximage[i][j], yimage[i][j], A);
+			}
+		}
+	}
+
+	// Threshold voting array
+	for (i = 0; i < 180; i++)
+	{
+		for (j = 0; j < 400; j++)
+		{
+			if (A[i][j] > 400)
+			{
+				printf("theta: %d, rho: %d, votes: %d\n", i, (j - 200) * 4, A[i][j]);
+			}
+		}
+	}
+
+	// Generate the line image
+	clear(lineimage);
+	draw_line(lineimage, 13, 312);
+	draw_line(lineimage, 51, -172);
+	draw_line(lineimage, 129, -300);
+
+
 	/* Write SGM to a new image */
 	strcpy( ifilename, filename[0] );
 	if (!( fp = fopen( strcat( ifilename, "-s.ras" ), "wb" ) ))
@@ -101,10 +139,62 @@ int main( int argc, char **argv )
 	for ( i = 0 ; i < ROWS ; i++ ) fwrite( bimage[i], 1, COLUMNS, fp );
 	fclose( fp );
 
+	/* Write the line image to a new image */
+	strcpy( ifilename, filename[0] );
+	if (!( fp = fopen( strcat( ifilename, "-h.ras" ), "wb" ) ))
+	{
+		fprintf( stderr, "error: could not open %s\n", ifilename );
+		exit( 1 );
+	}
+	fwrite( head, 4, 8, fp );
+	for ( i = 0 ; i < ROWS ; i++ ) fwrite( lineimage[i], 1, COLUMNS, fp );
+	fclose( fp );
+
+	printf( "Maximum of Gx, Gy, SGM\n" );
 	printf( "%d %d %d\n", max[0], max[1], max[2] );
 
 
 	return 0;
+}
+
+
+void draw_line(unsigned char image[ROWS][COLUMNS], int theta, int rho)
+{
+	int i, x;
+
+	// generate the points for the line
+	for (i = 0; i < ROWS; i++)
+	{
+		// use line equation to generate x for every y
+		x = ((float)i * cosf((float)theta * (PI / 180.0)) - (float)rho) / sinf((float)theta * (PI / 180.0));
+
+		// checks if x is on the image
+		if (x > 0 && x < 640)
+		{
+			// plots the point
+			image[i][x] = 255;
+		}
+	}
+}
+
+
+void hough(int x, int y, int dedx, int dedy, int array[180][400])
+{
+	float theta, rho;	// line parameters
+	int theta_index, rho_index;	// voting indicies
+
+	/* for a given point calculate rho for any given theta */
+	for (theta = 0; theta < 180; theta++)
+	{
+		/* calculate rho */
+		rho = ((float)y * cosf(theta * (PI / 180.0))) - ((float)x * sinf(theta * (PI / 180.0)));
+
+		/* vote for this line */
+		theta_index = (int)theta;
+		rho_index = (int)((rho / 4.0) + 200.0);
+
+		array[theta_index][rho_index]++;
+	}
 }
 
 
